2cb0f54d9256d7cd17d420222fc523e13fde706d
iuh20065911 <85084271+iuh20065911@users.noreply.github.com>
2024-03-31 09:31:52 +0700

    Update start.js

--- start.js
+++ start.js
@@ -5,22 +5,22 @@ const { query } = require("./data.js");
 
 app.use(express.json());
 
-app.post("/api/books", async (req, res) => {
+app.post("/api20065911/books", async (req, res) => {
   const { title, author } = req.body;
   console.info(`Adding book with title ${title} by author ${author}`);
 
   const result = await query(
-    "INSERT INTO tblbook (title, author) VALUES ($1, $2) RETURNING id",
+    "INSERT INTO tblbookIUH20065911 (title, author) VALUES ($1, $2) RETURNING id",
     [title, author]
   );
 
   res.json({ id: result[0].id });
 });
 
-app.get("/api/books", async (req, res) => {
+app.get("/api20065911/books", async (req, res) => {
   console.info(`Getting all books`);
 
-  const books = await query("SELECT * FROM tblbook");
+  const books = await query("SELECT * FROM tblbookIUH20065911");
   res.json(books);
 });
 
